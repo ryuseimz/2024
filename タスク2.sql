a14e6dfacb7c85cc82a240d577e54cf8a9de3838
ryuseimz <79738211+ryuseimz@users.noreply.github.com>
2024-10-02 19:11:22 +0900

    Update タスク2.sql

--- タスク2.sql
+++ タスク2.sql
@@ -2,10 +2,10 @@ SELECT
 	movie_id,
 	b.title,
 	b.release_date,
-	count(distinct a.id) as mark_count
-from marks a
-LEFT JOIN movies AS b ON a.movie_id = b.id 
-where (a.created_at + interval '9h')::date between '2024-08-01' and '2024-08-31'
-group by 1, 2, 3
-order by 4 desc
-limit 100;
+	count(DISTINCT a.id) AS mark_count
+FROM marks a
+LEFT JOIN movies AS b ON a.movie_id = b.id
+WHERE (a.created_at + interval '9h')::date BETWEEN '2024-08-01' AND '2024-08-31'
+GROUP BY 1, 2, 3
+ORDER BY 4 DESC
+LIMIT 100;
